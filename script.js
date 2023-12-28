0f50a2a1b377ce6327365e80c521173db78e6d5c
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-29 01:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function makeid(l) {
   // write your code here
+	function generateRandom() {
+		return parseInt(Math.random() * 100);
+	}
+
+	let ans = "";
+	for(let i=0; i<l; i++){
+		let charCode = generateRandom();
+    
+		if((48 <= charCode &&  charCode <= 57) || (65 <= charCode && charCode <= 90) || (97 <= charCode && charCode <=99)) {
+			
+      ans += String.fromCharCode(charCode);
+		}else {
+      
+			ans += String.fromCharCode((99 + (charCode % 23)));		
+		}
+	}
+ 
+	return ans;
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l));  
